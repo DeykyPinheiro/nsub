506f7fc4a9e25d86c0b11fd5cca2a58ccba1df4d
DeykyPinheiro <pinheiromikael96@gmail.com>
2025-11-03 21:41:46 -0300

    initial commit

---
+++ nsub/src/main/java/com/nsub/nsub/NsubApplication.java
@@ -0,0 +1,14 @@
+package com.nsub.nsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsubApplication.class, args);
+        System.out.println("teste");
+	}
+
+}
---
+++ nsub/src/test/java/com/nsub/nsub/NsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsub.nsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
