2e9cfbe4fba85ab318fe3ca60eeb68a249c6c2b7
DeykyPinheiro <pinheiromikael96@gmail.com>
2025-11-05 20:11:02 -0300

    feat: automatic reloading in Docker Compose

--- nsub/src/main/java/com/nsub/nsub/NsubApplication.java
+++ nsub/src/main/java/com/nsub/nsub/NsubApplication.java
@@ -2,13 +2,22 @@ package com.nsub.nsub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NsubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NsubApplication.class, args);
-        System.out.println("teste");
-	}
+        System.out.println("work work work");
 
+    }
+
+
+    @GetMapping("/")
+    public String hello() {
+        return "it workkksss??!";
+    }
 }
---
+++ nsub/src/main/java/model/User.java
@@ -0,0 +1,8 @@
+package model;
+
+public class User {
+    private  int Id;
+    private  String Name;
+    private  String Email;
+    private  String Passwordaaa;
+}
